a0ceb7605b4d865b47c09374f00fce2c7de97c19
Renato Groffe <renato.groffe@yahoo.com.br>
2021-03-14 23:19:26 -0300

    .NET 5 + ASP.NET Core + REST API + Serilog + Elasticsearch
    .NET 5 + ASP.NET Core + REST API + Serilog + Elasticsearch

---
+++ APIFinancas/Controllers/CalculoFinanceiroController.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using APIFinancas.Models;
+
+namespace APIFinancas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculoFinanceiroController : ControllerBase
+    {
+        private readonly ILogger<CalculoFinanceiroController> _logger;
+
+        public CalculoFinanceiroController(ILogger<CalculoFinanceiroController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("juroscompostos")]
+        [ProducesResponseType(typeof(Emprestimo), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(FalhaCalculo), (int)HttpStatusCode.BadRequest)]
+        public ActionResult<Emprestimo> Get(
+            double valorEmprestimo, int numMeses, double percTaxa)
+        {
+            _logger.LogInformation(
+                "Recebida nova requisição|" +
+               $"Valor do empréstimo: {valorEmprestimo}|" +
+               $"Número de meses: {numMeses}|" +
+               $"% Taxa de Juros: {percTaxa}");
+
+            if (valorEmprestimo <= 0)
+                return GerarResultParamInvalido("Valor do Empréstimo");
+
+            if (numMeses <= 0)
+                return GerarResultParamInvalido("Número de Meses");
+
+            if (percTaxa <= 0)
+                return GerarResultParamInvalido("Percentual da Taxa de Juros");
+
+            double valorFinalJuros =
+                CalculoFinanceiro.CalcularValorComJurosCompostos(
+                    valorEmprestimo, numMeses, percTaxa);
+            _logger.LogInformation($"Valor Final com Juros: {valorFinalJuros}");
+            LogStatusHttpRequest(HttpStatusCode.OK);
+
+            return new Emprestimo()
+            {
+                ValorEmprestimo = valorEmprestimo,
+                NumMeses = numMeses,
+                TaxaPercentual = percTaxa,
+                ValorFinalComJuros = valorFinalJuros
+            };
+        }
+
+        private BadRequestObjectResult GerarResultParamInvalido(
+            string nomeCampo)
+        {
+            var erro = $"O {nomeCampo} deve ser maior do que zero!";
+            _logger.LogError(erro);
+
+            LogStatusHttpRequest(HttpStatusCode.BadRequest);
+
+            return new BadRequestObjectResult(
+                new FalhaCalculo() { Mensagem = erro });
+        }
+
+        private void LogStatusHttpRequest(HttpStatusCode status)
+        {
+            var codStatus = (int)status;
+            var mensagem = $"/calculofinanceiro/juroscompostos {codStatus} {status}";
+
+            if (codStatus >= 400)
+                _logger.LogError(mensagem);
+            else
+                _logger.LogInformation(mensagem);
+        }
+    }
+}
---
+++ APIFinancas/Program.cs
@@ -0,0 +1,56 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace APIFinancas
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                var hostBuilder = CreateHostBuilder(args).Build();
+                Serilog.Log.Information("Iniciando Web Host");
+                hostBuilder.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Fatal(ex, "Host encerrado inesperadamente");
+                return 1;
+            }
+            finally
+            {
+                Serilog.Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    Serilog.Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .WriteTo.Elasticsearch(
+                            options:
+                                new ElasticsearchSinkOptions(
+                                    new Uri(settings["Elasticsearch:Uri"]))
+                                {
+                                    AutoRegisterTemplate = true,
+                                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                                    IndexFormat = "apifinancas-{0:yyyy.MM}"
+                                })
+                        .WriteTo.Console()
+                        .CreateLogger();
+                })
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
